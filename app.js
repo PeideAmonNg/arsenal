de678dd079c555bafcc6dcafdc0c98dba67d26a2
Amon Peide Ng <peide.amon.ng@gmail.com>
2017-12-15 11:35:26 +1300

    games and goals stats

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 
 var request = require('request');
 var prettyjson = require('prettyjson');
+var converter = require('number-to-words');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,15 +43,87 @@ app.get('/clubs', function(req, res, next) {
   //   res.end()
     res.render('index', {title: "EPL", epl: JSON.parse(body)})
 	})
-
 });
 
-app.get('/matches/:club', function(req, res, next) {
+app.get('/api/matches/:club/:season', function(req, res, next) {
+  var s = req.params.season;
+
+  request('https://raw.githubusercontent.com/opendatajson/football.json/master/' + s +'/en.1.json', function (error, response, body){
+
+    var rounds = JSON.parse(body).rounds;
+    var stats = getResults(rounds, req.params.club);
+
+    var o = [];
+    for(var key in stats){
+      o.push({type: key, number: stats[key]});
+    }
+    
+    // res.send(o);
+
+    var matches = {}
+
+    var homeGoals = 0, awayGoals = 0;
+
+    rounds.forEach(function(round){
+      round.matches.forEach(function(match){
+
+        if(match.team1.key == req.params.club){
+          homeGoals += match.score1;
+        }
+
+        if (match.team2.key == req.params.club){
+          awayGoals += match.score2;
+        }
+
+      });
+    });
+    var p = [{type: "home", number: homeGoals}, {type: "away", number: awayGoals}];
+
+    var q = {games: o, goals: p};
+
+    res.send(q);
+
+  });
+});
+
+app.get('/api/goals/:club/:season', function(req, res, next) {
+  var s = req.params.season;
+
+  request('https://raw.githubusercontent.com/opendatajson/football.json/master/' + s +'/en.1.json', function (error, response, body){
+
+    var rounds = JSON.parse(body).rounds;
+    var matches = {}
+
+    var homeGoals = 0, awayGoals = 0;
+
+    rounds.forEach(function(round){
+      round.matches.forEach(function(match){
+
+        if(match.team1.key == req.params.club){
+          homeGoals += match.score1;
+        }
+
+        if (match.team2.key == req.params.club){
+          awayGoals += match.score2;
+        }
+
+      });
+    });
+    var o = [{type: "home", number: homeGoals}, {type: "away", number: awayGoals}];
+    
+    res.send(o);
+
+  });
+});
+
+app.get('/matches/:club/:season', function(req, res, next) {
   // request({
   //   uri: 'https://raw.githubusercontent.com/opendatajson/football.json/master/2015-16/en.1.clubs.json'
   var start = Date.now()
 
-  request('https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/en.1.json', function (error, response, body){
+  var s = req.params.season;
+
+  request('https://raw.githubusercontent.com/opendatajson/football.json/master/' + s +'/en.1.json', function (error, response, body){
     // var o = JSON.parse(body);
   //   o.clubs.forEach(function(e){
   //     res.write(e.name +  "\n")
@@ -58,42 +132,113 @@ app.get('/matches/:club', function(req, res, next) {
 
     
     var rounds = JSON.parse(body).rounds;
-    var opponents = []
+    var matches = {}
 
     rounds.forEach(function(round){
       round.matches.forEach(function(match){
 
         if(match.team1.key == req.params.club){
-          var opp = match.team2;
+          match.name = round.name;
+
+          if(!(match.team2.key in matches)){
+            matches[match.team2.key] = {}
+            matches[match.team2.key].aggGoalsFor = match.score1
+            matches[match.team2.key].aggGoalsAgainst = match.score2
+            matches[match.team2.key].matches = []
+          }else{
+            matches[match.team2.key].aggGoalsFor += match.score1
+            matches[match.team2.key].aggGoalsAgainst += match.score2
+          }
+
+          matches[match.team2.key].matches.push(match)
+          var saved = match
         }
 
         if (match.team2.key == req.params.club){
-          var opp = match.team1;
+          match.name = round.name
+
+          if(!(match.team1.key in matches)){
+            matches[match.team1.key] = {}
+            matches[match.team1.key].aggGoalsFor = match.score2
+            matches[match.team1.key].aggGoalsAgainst = match.score1
+            matches[match.team1.key].matches = []
+          }else{
+            matches[match.team1.key].aggGoalsFor += match.score2
+            matches[match.team1.key].aggGoalsAgainst += match.score1
+          }
+
+          matches[match.team1.key].matches.push(match)
+          var saved = match
         }
 
-        if(opp){
-          // opponents.push(JSON.stringify(opp));
-          opponents.push(opp);
-        }
+        // if(saved){
+        //   matches.push(match);
+        // }
 
       });
     });
 
-    var oo = {opponents: opponents};
-    console.log(prettyjson.render(oo, {noColor: true}))    
+    var stats = getResults(rounds, req.params.club);
+
+
+    // for(var k in matches){
+    //   var match = matches[k]
+
+      // if(match.aggGoalsFor > match.aggGoalsAgainst){
+      //   stats.win++;
+      // }else if(match.aggGoalsFor < match.aggGoalsAgainst){
+      //   stats.lose++;
+      // }else{
+      //   stats.draw++;
+      // }
+    // }
+
+    stats.matches = matches;
+
+    // var oo = {opponents: opponents};
+    // console.log(prettyjson.render(oo, {noColor: true}))    
 
 
     // opponents.forEach(function(opp){
     //   res.write(prettyjson.render(opp) + "\n");
     //   console.log(prettyjson.render(opp, {noColor: true}))
     // });
-    res.write(prettyjson.render(oo, {noColor: true}));
-    res.end();
-    // res.send(body)
-    // res.send(matches)
+
+    var seasons = {seasons: ['2015-16', '2014-15', '2013-14', '2012-13', '2011-12']};
+    res.render('matches', {club: req.params.club, season: req.params.season, seasons: seasons, stats: stats, converter: converter});
   })
 
 });
+
+function getResults(rounds, club){
+  var stats = {win: 0, lose: 0, draw: 0}
+
+    rounds.forEach(function(round){
+      round.matches.forEach(function(match){
+        if(match.team1.key == club){
+          if(match.score1 > match.score2){
+            stats.win++;
+          }else if(match.score2 > match.score1){
+            stats.lose++;
+          }else{
+            stats.draw++;
+          }
+        }
+
+        if(match.team2.key == club){
+          if(match.score1 > match.score2){
+            stats.lose++;
+          }else if(match.score2 > match.score1){
+            stats.win++;
+          }else{
+            stats.draw++;
+          }
+        }
+      });
+    });
+
+    return stats;
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,120 @@
-var d3 = require("d3");
+console.log("gooner")
+
+var games;
+var goals;
+
+var arr = window.location.href.split('/');
+var club = arr[arr.length - 2];
+var season = arr[arr.length - 1];
+
+var xmlhttp = new XMLHttpRequest();
+var url = "/api/matches/" + club + "/" + season + "";
+
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    console.log("data");
+    console.log(data);
+    games = data.games;
+    goals = data.goals;
+    visualize();
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+// var path = "/api/" + window.location.href.replace(/^(?:\/\/|[^\/]+)*\//, "");
+// console.log(path);
+
+function visualize(){
+  console.log("games");
+  console.log(games);
+  console.log(goals);
+  var svg = d3.select("#allGames"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height"),
+      radius = Math.min(width, height) / 2,
+      g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  // var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  function color(c){ return {lose:"#E8A4A4", draw: "#E87878",win:"#E83737"}[c]; };
+
+  var pie = d3.pie()
+      .sort(function(x){ if(x.type == 'draw'){ return 0; }else if(x.type == 'win'){return 1;}else{ return -1;} })
+      .value(function(d) { return d.number; });
+
+  var path = d3.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+  var label = d3.arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
+
+  var data = games;
+    console.log(data)
+    var arc = g.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+        .attr("class", "arc");
+
+    arc.append("path")
+        .attr("d", path)
+        .attr("fill", function(d) { return color(d.data.type); });
+
+    arc.append("text")
+        .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+        .attr("dy", "0.35em")
+        .text(function(d) { return d.data.type + " " + d.data.number; });
+
+  var svg = d3.select("#allGoals"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height"),
+      radius = Math.min(width, height) / 2,
+      g2 = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  // var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+  function color2(c){ return {away:"#E8A4A4", draw: "#E87878",home:"#E83737"}[c]; };
+
+  var pie2 = d3.pie()
+      .sort(function(x, y){ return x.type > y.type; })
+      .value(function(d) { return d.number; });
+
+  var path2 = d3.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+  var label2 = d3.arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
+
+  var arr = window.location.href.split('/');
+  var club = arr[arr.length - 2];
+  var season = arr[arr.length - 1];
+
+  var data = goals;
+    console.log(data)
+    var arc = g2.selectAll(".arc")
+      .data(pie2(data))
+      .enter().append("g")
+        .attr("class", "arc");
+
+    arc.append("path")
+        .attr("d", path2)
+        .attr("fill", function(d) { return color2(d.data.type); });
+
+
+    arc.append("text")
+        .attr("transform", function(d) { return "translate(" + label2.centroid(d) + ")"; })
+        .attr("dy", "0.35em")
+        .text(function(d) { return d.data.type + " " + d.data.number; });
+
+    var totalGoals = 0;
+    data.forEach(function(g){
+      totalGoals += g.number;
+    });
+
+    document.getElementById("allGoalsTitle").innerHTML += " (" + totalGoals + ")";
+  
+}
+
